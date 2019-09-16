010b012f3746d03a0edc32f015c9bf21354d183c
Christopher Harrison <ch12@sanger.ac.uk>
2019-09-16 15:34:48 +0100

    Base graph interface

--- lib/planning/graph/types.py
+++ lib/planning/graph/types.py
@@ -17,19 +17,116 @@ You should have received a copy of the GNU General Public License along
 with this program. If not, see https://www.gnu.org/licenses/
 """
 
-from abc import ABCMeta, abstractmethod
+# We need postponed annotation evaluation for our recursive definitions
+# https://docs.python.org/3/whatsnew/3.7.html#pep-563-postponed-evaluation-of-annotations
+from __future__ import annotations
+
+from abc import ABCMeta
+from copy import copy
 
 from common import types as T
 
 
-class Vertex(metaclass=ABCMeta):
+class VertexNotInGraph(BaseException):
+    """ Raised when a vertex does not exist in the given graph """
+
+class NoRouteFound(BaseException):
+    """ Raised when no route can be found matching the itinerary """
+
+
+_T = T.TypeVar("_T")
+
+class _Carrier(T.Generic[_T], metaclass=ABCMeta):
+    """ Generic carrier abstract base class """
+    # NOTE While this is a container, in the semantic sense, it is
+    # different to Python's notion of a container (per typing.Container)
+    _payload:_T
+
+    @property
+    def payload(self) -> _T:
+        return self._payload
+
+    @payload.setter
+    def payload(self, value:_T) -> None:
+        self._payload = value
+
+
+class Vertex(_Carrier[T.Any], metaclass=ABCMeta):
     """ Vertex abstract base class """
 
 
-class Edge(metaclass=ABCMeta):
+class Edge(_Carrier[T.Any], T.Container[Vertex], metaclass=ABCMeta):
     """ Edge abstract base class """
+    _vertices:T.Tuple[Vertex, Vertex]
+    _directed:bool
     _cost:T.Number
 
+    def __init__(self, a:Vertex, b:Vertex, *, directed:bool = False) -> None:
+        self._vertices = (a, b)
+        self._directed = directed
 
-class Graph(metaclass=ABCMeta):
+    def __contains__(self, needle:Vertex) -> bool:
+        return needle in self._vertices
+
+    @property
+    def a(self) -> Vertex:
+        a, _ = self._vertices
+        return a
+
+    @property
+    def b(self) -> Vertex:
+        _, b = self._vertices
+        return b
+
+    @property
+    def is_directed(self) -> bool:
+        return self._directed
+
+    @property
+    def cost(self) -> T.Number:
+        return self._cost
+
+    @cost.setter
+    def cost(self, value:T.Number) -> None:
+        self._cost = value
+
+
+class Graph(T.Container[Edge], metaclass=ABCMeta):
     """ Graph abstract base class """
+    # NOTE Our graph is a container of edges; contrary to definition, we
+    # do not consider unconnected vertices to be in the graph
+    _edges:T.List[Edge]
+
+    def __init__(self) -> None:
+        self._edges = []
+
+    def __contains__(self, needle:Vertex) -> bool:
+        """ Does the graph contain a given vertex? """
+        # NOTE We openly flaunt the interface of typing.Container, as
+        # we're more interested in the vertices, rather than the edges
+        return any(needle in haystack for haystack in self._edges)
+
+    def __iadd__(self, edge:Edge) -> Graph:
+        """ Add an edge to a graph """
+        self._edges.append(edge)
+        return self
+
+    def __add__(self, graph:Graph) -> Graph:
+        """ Return the union of two graphs """
+        union = copy(graph)
+        for edge in self._edges:
+            union += edge
+
+        return union
+
+    def neighbours(self, vertex:Vertex) -> T.Iterable[Edge]:
+        """ Return all immediate neighbours, by edge, of a vertex """
+        if vertex not in self:
+            raise VertexNotInGraph(f"Vertex {vertex} is not in Graph {self}")
+
+        for needle in self._edges:
+            if vertex is needle.a:
+                yield needle
+
+            elif not needle.is_directed and vertex is needle.b:
+                yield needle
