084e329c84f2cc9886e2c620f193875ee8ceba33
Christopher Harrison <ch12@sanger.ac.uk>
2019-09-23 17:09:54 +0100

    Stub for POSIX to iRODS transfer

---
+++ lib/planning/posix_to_irods.py
@@ -0,0 +1,31 @@
+"""
+Copyright (c) 2019 Genome Research Limited
+
+Author: Christopher Harrison <ch12@sanger.ac.uk>
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see https://www.gnu.org/licenses/
+"""
+
+from .posix import POSIXFilesystem
+# from .irods import iRODSFilesystem
+from .templating import transfer_script
+from .transfer import TransferRoute
+
+
+# FIXME This is for testing only
+iRODSFilesystem = POSIXFilesystem
+
+posix_to_irods = TransferRoute(POSIXFilesystem(), iRODSFilesystem(), templating=transfer_script("""
+    # TODO Script in here!
+"""))
