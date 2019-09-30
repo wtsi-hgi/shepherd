24ece1a03724142da08d21171b9200d48c9aed3f
Christopher Harrison <ch12@sanger.ac.uk>
2019-09-30 16:00:29 +0100

    View of transfer tasks to complete

--- lib/state/native/schema.sql
+++ lib/state/native/schema.sql
@@ -33,16 +33,12 @@ create index if not exists jobs_pk on jobs(id);
 
 create table if not exists data (
   id          integer  primary key,
-  job         integer  references jobs(id),
   filesystem  text     not null,
-  location    text     not null,
-
-  unique (job, filesystem, location)
+  location    text     not null
 );
 
 create index if not exists data_pk on data(id);
-create index if not exists data_key on data(id, job);
-create index if not exists data_location on data(job, filesystem, location);
+create index if not exists data_location on data(filesystem, location);
 
 create table if not exists checksums (
   data       integer  primary key references data(id),
@@ -72,28 +68,95 @@ create index if not exists metadata_key on metadata(data, key);
 
 create table if not exists tasks (
   id          integer  primary key,
+  job         integer  references jobs(id),
   source      integer  references data(id),
   target      integer  references data(id) check (source != target),
   script      text     not null,
   dependency  integer  null references tasks(id) check (dependency is null or dependency != id),
 
-  unique (target),
-  unique (source, target)
+  unique (job, target),
+  unique (job, source, target)
 );
 
 create index if not exists tasks_pk on tasks(id);
+create index if not exists tasks_job on tasks(job);
 
 create table if not exists attempts (
-  task       integer  primary key references tasks(id),
+  task       integer  references tasks(id),
   attempt    integer  not null,
   start      integer  not null default (strftime('%s', 'now')),
   finish     integer  check (finish is null or finish >= start),
   exit_code  integer  default (null),
 
+  primary key (task, attempt),
   unique (task, attempt)
-) without rowid;
+);
 
 create index if not exists attempts_task on attempts(task);
 create index if not exists attempts_attempt on attempts(task, attempt);
+create index if not exists attempts_succeeded on attempts(task, attempt, exit_code) where exit_code = 0;
+create index if not exists attempts_failed on attempts(task, attempt, exit_code) where exit_code is not null and exit_code != 0;
+
+-- TODO Use recursive CTEs?
+create view if not exists to_transfer as
+  with failed as (
+    select  attempts.task,
+            attempts.attempt
+    from    attempts
+    where   attempts.exit_code is not null
+    and     attempts.exit_code != 0
+  ),
+  fail_count as (
+    select   failed.task,
+             count(failed.attempt) as num_failed
+    from     failed
+    group by failed.task
+  ),
+  succeeded as (
+    select  attempts.task,
+            attempts.attempt
+    from    attempts
+    where   attempts.exit_code = 0
+  ),
+  todo as (
+    select    failed.task as task,
+              fail_count.num_failed
+    from      failed
+    join      fail_count
+    on        fail_count.task = failed.task
+    left join succeeded
+    on        succeeded.task    = failed.task
+    and       succeeded.attempt > failed.attempt
+    where     succeeded.task is null
+
+    union all
+
+    select    tasks.id as task,
+              0 as num_failed
+    from      tasks
+    left join attempts
+    on        attempts.task = tasks.id
+    where     attempts.task is null
+  )
+  select    ready.task,
+            tasks.job,
+            source.filesystem as source_filesystem,
+            source.location   as source_location,
+            target.filesystem as target_filesystem,
+            target.location   as target_location,
+            tasks.script
+  from      todo as ready
+  join      tasks
+  on        tasks.id = ready.task
+  left join todo as pending
+  on        pending.task = tasks.dependency
+  join      jobs
+  on        jobs.id = tasks.job
+  join      data as source
+  on        source.id = tasks.source
+  join      data as target
+  on        target.id = tasks.target
+  where     pending.task is null
+  and       ready.num_failed < jobs.max_attempts;
 
 commit;
