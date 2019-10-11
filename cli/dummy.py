50320a463b6d6508bcb611749d93ccf2d761dd3c
Christopher Harrison <ch12@sanger.ac.uk>
2019-10-11 16:58:27 +0100

    Very rudimentary, serial worker

--- cli/dummy.py
+++ cli/dummy.py
@@ -17,37 +17,51 @@ You should have received a copy of the GNU General Public License along
 with this program. If not, see https://www.gnu.org/licenses/
 """
 
+from time import sleep
+
 from common import types as T
 from common.models.filesystems import POSIXFilesystem, iRODSFilesystem
 from lib.planning.transformers import strip_common_prefix, last_n_components, prefix, telemetry, debugging
 from lib.planning.route_factories import posix_to_irods_factory
+from lib.state.types import JobStatus, DataNotReady
 from lib.state.native import NativeJob, create_root
 
-def main(*args:str) -> None:
-    fofn, *_ = args
 
-    lustre = POSIXFilesystem(name="Lustre", max_concurrency=50)
-    irods = iRODSFilesystem()
+_FS = {
+    "Lustre": POSIXFilesystem(name="Lustre", max_concurrency=50),
+    "iRODS":  iRODSFilesystem(name="iRODS")
+}
 
-    transfer = posix_to_irods_factory(lustre, irods)
+
+def print_status(status:JobStatus) -> None:
+    print(f"* Pending:   {status.pending}")
+    print(f"* Running:   {status.running}")
+    print(f"* Failed:    {status.failed}")
+    print(f"* Succeeded: {status.succeeded}")
+
+
+def create_state_from_fofn(fofn:T.Path) -> None:
+    """ Lustre to iRODS state from FoFN """
+    transfer = posix_to_irods_factory(_FS["Lustre"], _FS["iRODS"])
     transfer += strip_common_prefix
-    transfer += prefix(T.Path("/here/is/a/prefix"))
+    transfer += prefix(T.Path("/here/is/a/prefix/for/humgen/shepherd_testing"))
     transfer += last_n_components(4)
     transfer += debugging
     transfer += telemetry
 
     state_root = create_root(T.Path("."))
-    state = NativeJob(state_root)
+    job = NativeJob(state_root)
+    job.filesystem_mapping = _FS
 
     print(f"State:  {state_root}")
-    print(f"Job ID: {state.job_id}")
+    print(f"Job ID: {job.job_id}")
 
     tasks = 0
-    files = lustre._identify_by_fofn(T.Path(fofn))
+    files = _FS["Lustre"]._identify_by_fofn(fofn)
     for task in transfer.plan(files):
         print(("=" if tasks == 0 else "-") * 72)
 
-        state += task
+        job += task
         tasks += 1
 
         print(f"Source: {task.source.filesystem} {task.source.address}")
@@ -55,9 +69,55 @@ def main(*args:str) -> None:
 
     print("=" * 72)
     print(f"Added {tasks} tasks to job:")
+    print_status(job.status)
 
-    status = state.status
-    print(f"* Pending:   {status.pending}")
-    print(f"* Running:   {status.running}")
-    print(f"* Failed:    {status.failed}")
-    print(f"* Succeeded: {status.succeeded}")
+
+def run_state(state_root:str, job_id:int) -> None:
+    """ Run through tasks in state database """
+    job = NativeJob(T.Path(state_root), job_id=job_id, force_restart=True)
+    job.filesystem_mapping = _FS
+
+    print(f"State:  {state_root}")
+    print(f"Job ID: {job.job_id}")
+    print_status(job.status)
+
+    tasks = 0
+    while job.status:
+        try:
+            task = next(job)
+
+        except StopIteration:
+            break
+
+        except DataNotReady:
+            print("Not ready; sleeping...")
+            sleep(60)
+            continue
+
+        print(("=" if tasks == 0 else "-") * 72)
+        print(f"Source: {task.source.filesystem} {task.source.address}")
+        print(f"Target: {task.target.filesystem} {task.target.address}")
+
+        # TODO Check exit status and update state
+        task()
+
+        tasks += 1
+
+    print("=" * 72)
+    print("Done")
+    print_status(job.status)
+
+
+def main(*args:str) -> None:
+    mode, *mode_args = args
+
+    delegate = {
+        "fofn": create_state_from_fofn,
+        "exec": run_state
+    }
+
+    if mode not in delegate:
+        print(f"No such mode {mode}!")
+        exit(1)
+
+    delegate[mode](*mode_args)
