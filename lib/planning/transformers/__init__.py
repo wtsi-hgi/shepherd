3d6d104a750ddf3716dbbed5886732b7aca707b3
Filip Makosza <fm12@sanger.ac.uk>
2019-10-30 14:13:11 +0000

    Added transformations to init

--- lib/planning/transformers/__init__.py
+++ lib/planning/transformers/__init__.py
@@ -1,3 +1,5 @@
 from .prefix import prefix
 from .strip import strip_common_prefix, last_n_components
 from .wrappers import telemetry, debugging
+from .percent_encode import percent_encode
+from .character_translate import character_translator
