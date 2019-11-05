946df96c020521828bcbe0d22823e88d647bc749
Christopher Harrison <ch12@sanger.ac.uk>
2019-11-05 18:58:41 +0000

    Pinned down templating API

--- common/templating/jinja2.py
+++ common/templating/jinja2.py
@@ -19,6 +19,7 @@ with this program. If not, see https://www.gnu.org/licenses/
 
 import os
 import re
+from dataclasses import dataclass
 from functools import partial
 
 from jinja2 import Environment, Template, meta
@@ -27,43 +28,64 @@ from .. import types as T
 from .types import BaseTemplating, Filter, TemplatingError, templating_factory
 
 
+@dataclass
+class _Template:
+    """ Internal template model """
+    source:str
+    variables:T.Set[str]
+    template:Template
+
+    def __init__(self, source:str, environment:Environment) -> None:
+        self.source = source
+
+        parsed = environment.parse(source)
+        self.variables = meta.find_undeclared_variables(parsed)
+
+        self.template = environment.from_string(source)
+
+    def render(self, **variables) -> str:
+        v = set(variables)
+        if v < self.variables or v.isdisjoint(self.variables):
+            missing = ", ".join(self.variables - v)
+            raise TemplatingError(f"Variables undefined for template: {missing}")
+
+        return self.template.render(**variables)
+
+
 class Jinja2Templating(BaseTemplating):
     """ Jinja2-based templating engine """
     _env:Environment
-    _templates:T.Dict[str, T.Tuple[str, Template]]
+    _templates:T.Dict[str, _Template]
 
     def __init__(self, **kwargs:T.Any) -> None:
         self._env = Environment(**kwargs)
         self._templates = {}
 
     @property
-    def templates(self) -> T.List[str]:
-        return list(self._templates.keys())
-
-    @property
-    def filters(self) -> T.List[str]:
-        return list(self._env.filters.keys())
+    def templates(self) -> T.Iterator[str]:
+        return iter(self._templates.keys())
 
     def add_template(self, name:str, template:str) -> None:
-        self._templates[name] = template, self._env.from_string(template)
+        self._templates[name] = _Template(template, self._env)
 
     def get_template(self, name:str) -> str:
-        template, _ = self._templates[name]
-        return template
+        return self._templates[name].source
 
-    def get_variables(self, name:str) -> T.Set[str]:
-        source = self._env.parse(self.get_template(name))
-        return meta.find_undeclared_variables(source)
+    def get_variables(self, name:str) -> T.Iterator[str]:
+        return iter(self._templates[name].variables)
+
+    @property
+    def filters(self) -> T.Iterator[str]:
+        return iter(self._env.filters.keys())
 
     def add_filter(self, name:str, fn:Filter) -> None:
         self._env.filters[name] = fn
 
-    def render(self, name:str, **tags:T.Any) -> str:
+    def render(self, name:str, **variables:T.Any) -> str:
         if name not in self._templates:
             raise TemplatingError(f"No such template {name}")
 
-        _, template = self._templates[name]
-        return template.render(**tags)
+        return self._templates[name].render(**variables)
 
 
 # Standard Jinja2-based templating factory and filters
