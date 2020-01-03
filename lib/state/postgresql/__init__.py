6b2574a8a3e553d1e0a6992829af8d18d646089e
Christopher Harrison <ch12@sanger.ac.uk>
2020-01-03 16:45:03 +0000

    Canonicalise PostgreSQL implemenation exports

---
+++ lib/state/postgresql/__init__.py
@@ -0,0 +1,5 @@
+from .db import PostgreSQL
+from .state import PGPhaseStatus as PhaseStatus, \
+                   PGJobStatus as JobStatus, \
+                   PGAttempt as Attempt, \
+                   PGJob as Job
